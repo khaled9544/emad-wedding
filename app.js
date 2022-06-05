a8b8095ce44a5c86992edc156299a95282690154
khaledneo <khaled@neo.ae>
2022-06-05 09:01:24 +0300

    Fixed the splash screen

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 var countDownDate = new Date("Jul 29, 2022 20:30:00").getTime();
 
 var map =
-[
-"&\#1632;","&\#1633;","&\#1634;","&\#1635;","&\#1636;",
-"&\#1637;","&\#1638;","&\#1639;","&\#1640;","&\#1641;"
-];
+  [
+    "&\#1632;", "&\#1633;", "&\#1634;", "&\#1635;", "&\#1636;",
+    "&\#1637;", "&\#1638;", "&\#1639;", "&\#1640;", "&\#1641;"
+  ];
 
-function getArabicNumbers(str)
-{
-    var newStr = "";
+function getArabicNumbers(str) {
+  var newStr = "";
 
-    str = String(str);
+  str = String(str);
 
-    for(i=0; i<str.length; i++)
-    {
-        newStr += map[parseInt(str.charAt(i))];
-    }
+  for (i = 0; i < str.length; i++) {
+    newStr += map[parseInt(str.charAt(i))];
+  }
 
-    return newStr;
+  return newStr;
 }
 
-var x = setInterval(function() {
+var x = setInterval(function () {
 
   var now = new Date().getTime();
-    
+
   var distance = countDownDate - now;
-    
+
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   document.getElementById("timer").innerHTML = `<span class='hours'>${getArabicNumbers(hours)}</span> `
-  + `<span class='minutes'>${getArabicNumbers(minutes)}</span>` + `<span class='seconds'>${getArabicNumbers(seconds)}</span>`;
+    + `<span class='minutes'>${getArabicNumbers(minutes)}</span>` + `<span class='seconds'>${getArabicNumbers(seconds)}</span>`;
 
   document.getElementById("remaining-days").innerHTML = `${getArabicNumbers(days)}`
-    
+
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("timer").innerHTML = "EXPIRED";
@@ -46,31 +44,37 @@ var x = setInterval(function() {
 document.getElementById("wedding-date").innerHTML = `${getArabicNumbers(29)} يوليو <br>,${getArabicNumbers(2022)}`;
 
 
-function getUrlVars()
-{
+function getUrlVars() {
   // console.log("hello")
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars;
+  var vars = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
+  return vars;
 }
 
 var person = getUrlVars()["any"];
-if(person) {
+if (person) {
   document.getElementById("number-of-invitees").innerHTML = `${getArabicNumbers(parseInt(person[3]))}`;
 } else {
   document.getElementById("number-of-invitees").innerHTML = `${getArabicNumbers(6)}`;
 }
 
-this.interval = setInterval(() => {
-  if(document.readyState === 'complete') {
-    // document.getElementById('main-container').style.display = 'block';
+// this.interval = setInterval(() => {
+//   if(document.readyState === 'complete') {
+//     // document.getElementById('main-container').style.display = 'block';
+//     document.getElementById('splash-screen').style.display = 'none';
+//     clearInterval(this.interval);
+//   }
+// }, 2000);
+
+$(document).ready(function () {
+  console.log(jQuery);
+  if (jQuery) {
     document.getElementById('splash-screen').style.display = 'none';
-    clearInterval(this.interval);
   }
-}, 2000);
+  // Handler for .ready() called.
+});
